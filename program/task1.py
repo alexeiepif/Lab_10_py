8261cbcbdad57656a5195576ed5d21e724595f1c
alexseiepif <aled2093746@gmail.com>
2023-11-30 13:34:22 +0300

    modifi task1

--- program/task1.py
+++ program/task1.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Решите задачу: подсчитайте количество гласных в строке, 
+# Решите задачу: подсчитайте количество гласных в строке,
 # введенной с клавиатуры с использованием множеств.
 
-
 if __name__ == "__main__":
-    vowels = {'a', 'e', 'i', 'o', 'u', 'y'}
-    vowels_rus = {'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'}
+    vowels_all = {'a', 'e', 'i', 'o', 'u', 'y',
+                  'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'}
 
     string = input("Введите строку: ")
 
-    count = 0
-
-    for char in string.lower():
-        if char in vowels or char in vowels_rus:
-            count += 1
+    count = len([i for i in string if i in vowels_all])
 
     print("Количество гласных в строке:", count)
